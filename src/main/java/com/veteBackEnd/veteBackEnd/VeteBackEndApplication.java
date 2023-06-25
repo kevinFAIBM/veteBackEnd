1fc2829cbde3640c8a5f3cb2cf41a380fa2bed19
kevinFAIBM <131728292+kevinFAIBM@users.noreply.github.com>
2023-06-24 18:04:16 -0600

    first commit

---
+++ src/main/java/com/veteBackEnd/veteBackEnd/VeteBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.veteBackEnd.veteBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeteBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeteBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/veteBackEnd/veteBackEnd/VeteBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veteBackEnd.veteBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeteBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
